52eed2471282d8949732ecc1a19ed0df8ce8588a
ahmet çiftçi <ciftciahmet70@hotmail.com>
2022-07-13 17:28:50 +0300

    Add project files.

---
+++ CoreKitaplik/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using CoreKitaplik.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CoreKitaplik.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var ktp = new List<Kitaplar>()
+            {
+                new Kitaplar(){Id=1,KitapAd="Satranç",Yazar="Zweig"},
+                new Kitaplar(){Id=2,KitapAd="Dr Ox'un Deneyi",Yazar="Jules Verne"},
+                new Kitaplar(){Id=3,KitapAd="Delifişek",Yazar="Vasconceulos"},
+                new Kitaplar(){Id=4,KitapAd="Kırmızı Bisiklet",Yazar="Muzaffer İzgü"},
+                new Kitaplar(){Id=5,KitapAd="Toros Canavarı",Yazar="Aziz Nesin"},
+            };
+            return View(ktp);
+        }
+        public IActionResult Deneme()
+        {
+            return View();
+        }
+        public IActionResult StatikTema()
+        {
+            return View();
+        }
+    }
+}
---
+++ CoreKitaplik/ViewComponents/YeniKitaplar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using CoreKitaplik.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace CoreKitaplik.ViewComponents
+{
+    public class YeniKitaplar:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var kitapListesi = new List<Kitaplar>
+            {
+                new Kitaplar
+                {
+                    Id=6,
+                    KitapAd="Korku",
+                    Yazar="Zweig",
+                },
+                new Kitaplar
+                {
+                    Id=7,
+                    KitapAd="Anna Karenina",
+                    Yazar="Tolstoy",
+                }
+            };
+            return View(kitapListesi);
+        }
+        
+    }
+}
